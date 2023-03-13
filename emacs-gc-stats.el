b2c73b2dc0c14c821f2850e2a2079c70ca94270d
Ihor Radchenko <yantar92@posteo.net>
2023-03-13 14:19:15 +0100

    Initial commit

---
+++ emacs-gc-stats.el
@@ -0,0 +1,30 @@
+;;; emacs-gc-stats.el --- Collect Emacs GC statistics  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Ihor Radchenko
+
+;; Author: Ihor Radchenko <yantar92@posteo.net>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+
+;; 
+;;; Code:
+
+
+
+(provide 'emacs-gc-stats)
+;;; emacs-gc-stats.el ends here
